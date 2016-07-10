ec6a697570ce65f7fcef2e78ff8c6eedeae1fb4d
Nathan Krantz-Fire <zippy7890@gmail.com>
2016-07-09 22:10:16 -0700

    Updated image.py to make it usable (also, it's meant to be run locally, not offline).

--- image.py
+++ image.py
@@ -1,4 +1,4 @@
-# This file ships with dronebot.
+# This file ships with dronebot and is meant to be run locally, not on the server.
 
 #  (c) Copyright 2016 Nathan Krantz-Fire (a.k.a zippynk). Some rights reserved.
 #  https://github.com/zippynk/dronebot
@@ -9,6 +9,8 @@
 
 from urllib import request
 import json
+import sys
+
 def world_map(server):
     url1 = "http://{0}:10407/update".format(server)
     load = json.loads(request.urlopen(url1).read().decode())
@@ -17,3 +19,6 @@ def world_map(server):
     else:
         url2 = "http://staticmap.openstreetmap.de/staticmap.php?center=0,0&zoom=2&size=1024x1024&maptype=mapnik&markers={0},{1},red-pushpin{2}".format(load["lat"],load["lon"],("|" + str(load["destinations"][0][0]) + "," + str(load["destinations"][0][1]) + ",lightblue1") if len(load["destinations"]) > 0 else "",("|" + str(load["destinations"][1][0]) + "," + str(load["destinations"][1][1]) + ",lightblue2") if len(load["destinations"]) > 1 else "",("|" + str(load["destinations"][2][0]) + "," + str(load["destinations"][2][1]) + ",lightblue3") if len(load["destinations"]) > 2 else "")
         return url2
+
+if __name__ == "__main__":
+    print world_map(input("Server? ") if len(sys.argv == 0) else sys.argv[1])
